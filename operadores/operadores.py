60e81f864ebcf3e4fbad717e35ead4ae9c45da46
Raphael Martins <raphael.m.nascimento6@aluno.senai.br>
2023-08-08 09:10:09 -0300

    exercios 03

--- operadores/operadores.py
+++ operadores/operadores.py
@@ -12,3 +12,25 @@ print(f"Divisão: { div }")
 print(f"Divisão de inteiros: { div_inteiros }")
 print(f"Cálculos dentro do print: { a * b }")
 print(f"Resto da divisão de 16 por a: { 16 % a }")
+
+print(f"a == b: { a == b }")
+print(f"a < 5: { a < 5 }")
+print(f"a > b: { a > b }")
+print(f"a <= 3: { a <= 3 }")
+print(f"a >= 4: { a >= 4 }")
+print(f"a != b: { a != b }")
+
+c = a != b
+print(f"c:{c}")
+print(f"tipo da var c: {type(c)}") #bool é o tipo booleano ou lógico
+
+# Operadores Lógicos
+logic1 = True
+logic2 = False
+print(f"type(logic1): {type(logic1)}")
+print(f"type(logic2): {type(logic2)}")
+
+print(logic1)
+print(not logic1)
+print(logic1 or logic2)
+print(logic1 and logic2)
